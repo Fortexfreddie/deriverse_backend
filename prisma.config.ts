c28d49531d2a32f6f8676a0475ef4f19e0ac3a18
Fortexfreddie <106601096+Fortexfreddie@users.noreply.github.com>
2026-02-18 23:07:59 +0100

    feat: prisma config update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,17 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    // Switch between local DB and Supabase Direct URL
+    url: isProduction 
+      ? process.env["DIRECT_URL"] 
+      : process.env["DATABASE_URL"], 
   },
 });
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,16 @@
 import "dotenv/config";
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client'; // or from your generated path
+import { PrismaClient } from '@prisma/client';
 
-// 1. Setup the standard Postgres connection pool
-const connectionString = `${process.env.DATABASE_URL}`;
+const isProduction = process.env.NODE_ENV === "production";
+
+// In production, use the Pooler URL (6543). Locally, use your standard DATABASE_URL.
+const connectionString = isProduction 
+  ? process.env.POOLER_URL 
+  : process.env.DATABASE_URL;
+
 const pool = new Pool({ connectionString });
-
-// 2. Setup the Driver Adapter (This is the new Prisma 7 requirement)
 const adapter = new PrismaPg(pool);
 
-// 3. Instantiate the client with the adapter
 export const prisma = new PrismaClient({ adapter });
